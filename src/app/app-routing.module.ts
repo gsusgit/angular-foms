ebca8a9d446ccd1f2ed8a4feff3ccefb13554b12
Jesus Fernandez <fernandezjodar.jesus@gmail.com>
2020-10-13 16:59:15 +0200

    Añadidas validaciones a formulario template

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {TemplateComponent} from './pages/template/template.component';
-import {ReactiveComponent} from './pages/reactive/reactive.component';
+import { TemplateComponent } from './pages/template/template.component';
+import { ReactiveComponent } from './pages/reactive/reactive.component';
 
 const routes: Routes = [
   {path: 'template', component: TemplateComponent},
--- src/app/pages/template/template.component.ts
+++ src/app/pages/template/template.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-template',
   templateUrl: './template.component.html',
   styleUrls: ['./template.component.css']
 })
-export class TemplateComponent implements OnInit {
-
+export class TemplateComponent {
+  usuario = {
+    nombre: '',
+    apellido: '',
+    email: ''
+  };
   constructor() { }
-
-  ngOnInit(): void {
+  guardar(form: NgForm): void {
+    if (form.valid) {
+      console.log(form);
+    }
   }
-
 }
